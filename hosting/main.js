d07132830ff87d8d6bd89839824a6003b45804ca
jonnilundy <jonathan.lundy@liferay.com>
2017-04-05 15:47:46 -0700

    add missing code

--- hosting/main.js
+++ hosting/main.js
@@ -3,7 +3,25 @@ var form = document.querySelector('form');
 form.addEventListener('submit', function(e) {
 	e.preventDefault();
 
-// Insert post method bellow
-
-// Insert post method above
+	WeDeploy
+		.url(`email.${DOMAIN}`)
+		.auth('0000000-0000-000-0000-0000000')
+		.form('from', form.from.value)
+		.form('to', form.to.value)
+		.form('subject', form.subject.value)
+		.form('message', form.subject.value)
+		.post()
+		.then(function(response) {
+			if (response.succeeded()) {
+				form.reset();
+				alert('Email sent! Wait a little bit until it arrives :)');
+				console.info('Email ID:', response.body());
+			}
+			else {
+				alert('Email was not sent');
+			}
+		})
+		.catch(function(error) {
+			alert('Oops, some error has happened.');
+		});
 });
